58134c54eb05a75e01a2c47af4296932f302b4b0
n_mo <nimo@mail.yo>
2026-01-11 01:05:34 +0700

    Платформы спавнятся, перехожу от платформы к платформе

--- Code/main.gd
+++ Code/main.gd
@@ -1,18 +1,47 @@
 extends Node2D
 
+
+@onready var player: Control = $Player
+
+
 var platform_preload: PackedScene = preload("res://Scenes/platform.tscn")
+var platfrom_1: Platform
+var platfrom_2: Platform
 
 
 func _ready() -> void:
-	pass
+	start_game()
 
 
-func _process(delta: float) -> void:
-	pass
+func start_game() -> void:
+	platfrom_1 = null
+	platfrom_2 = null
+	platfrom_1 = platform_preload.instantiate()
+	platfrom_2 = platform_preload.instantiate()
+	
+
+	add_child(platfrom_1)
+	platfrom_1.position.x = 0
+	add_child(platfrom_2)
+	
+	player.position.x = (platfrom_1.position.x + platfrom_1.get_size_x()) - 20
+
+
+func move_player_to_platform() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(player, "position:x", platfrom_2.position.x + platfrom_2.get_size_x() - 20, 0.5)
+	tween.finished.connect(spawn_next_platform)
+
+
+func spawn_next_platform() -> void:
+	var min_pos_x = platfrom_2.position.x + 200
+	var max_pos_x = platfrom_2.position.x + 420
+	platfrom_2 = platform_preload.instantiate()
+	platfrom_2.min_position_x = min_pos_x
+	platfrom_2.max_position_x = max_pos_x
+	add_child(platfrom_2)
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
-		print("ui_accept")
-		var platform: Platform = platform_preload.instantiate()
-		add_child(platform)
+		move_player_to_platform()
--- Code/platform.gd
+++ Code/platform.gd
@@ -3,27 +3,37 @@ class_name Platform extends Control
 @onready var center: ColorRect = $Center
 @onready var main: ColorRect = $Main
 
+var min_position_x = 200
+var max_position_x = 420
+
+var can_remove: bool = false
+
 
 func _ready() -> void:
-	print("im there")
 	random_position()
 	random_size()
 	center.position.x = (main.size.x - center.size.x) / 2
 	var tween = get_tree().create_tween()
 	tween.set_trans(Tween.TRANS_ELASTIC)
-	tween.tween_property(self, "position:y", 640, 0.3)
-	await get_tree().create_timer(1.0).timeout
-	print("timeout")
-	queue_free()
+	tween.tween_property(self, "position:y", 640, 0.5)
+	tween.finished.connect(func(): 
+		can_remove = true
+	)
 
 
 func random_position() -> void:
-	position.x = randi_range(200, 420)
+	position.x = randi_range(min_position_x, max_position_x)
 
 
 func random_size() -> void:
-	main.size.x = randi_range(40, 90)
+	if main:
+		main.size.x = randi_range(40, 90)
 
 
-func _process(delta: float) -> void:
-	pass
+func get_size_x() -> int:
+	return main.size.x
+
+
+func _on_visible_on_screen_enabler_2d_screen_exited() -> void:
+	if can_remove:
+		queue_free()
